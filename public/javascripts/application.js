0bd65263f7655f04b43b77d798c841ea1f483c98
Aaron Brice <aaron.brice@gmail.com>
2009-02-15 23:49:56 -0700

    Animated flash notice box

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+if (window.addEventListener) {
+  window.addEventListener("load", animate_flash, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", animate_flash);
+} else {
+  window.onload = animate_flash;
+}
+
+function animate_flash() {
+  if ($('flash_notice') != null) {
+    $('flash_notice').hide();
+    $('flash_notice').slideDown();
+    $('flash_notice').slideUp({delay: 3.0, queue: 'end'});
+  }
+}
+
