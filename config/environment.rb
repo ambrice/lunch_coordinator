d9a96ea18be215e923e47f9f6fcf405a1f5f34ae
Aaron Brice <aaron.brice@gmail.com>
2010-03-28 18:06:35 -0700

    Added camping gem

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   config.gem "rack"
   config.gem "test-spec"
+  config.gem "camping"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
